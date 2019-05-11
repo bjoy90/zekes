48f388a970fda0110b8320251f083cb5d062a581
Bethany Fannin <beth.joy126@gmail.com>
2019-05-11 11:39:07 -0400

    Move javascript code into its own javascript file js/script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+function newFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "block") {
+    x.style.display = "none";
+    } else {
+x.style.display = "block";
+}
+}
